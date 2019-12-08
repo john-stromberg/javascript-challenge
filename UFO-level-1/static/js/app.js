9dfb5390acbd87099963250474d58429c60c6561
Jack Stromberg <john.stromberg92@gmail.com>
2019-12-07 23:16:24 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,42 @@
 // from data.js
 var tableData = data;
 // select the table body
-var tbody = d3.select("#tbody");
+var tbody = d3.select("tbody");
 
-data.forEach(function(ufoFinder) {
-    console.log(ufoFinder);
+console.log(data);
+
+// loop and through and console log each UFO event
+data.forEach(function(ufoEvent) {
+    console.log(ufoEvent);
+});
+
+data.forEach(function(ufoEvent) {
+    console.log(ufoEvent);
     var row = tbody.append("tr");
-    Object.entries(ufoFinder).forEach(function([key, value]){
-        console.log(key,value);
-        var cell = tbody.append("td");
-        cell.text(value);
+    Object.entries(ufoEvent).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
     });
+  });
+ 
+
+
+
+
+
+
+// select the button
+var button = d3.select("#button");
+
+button.on("click", function(){
+    var inputElement = d3.select("#button");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(data);
+
+    var filteredData = data.filter(data => data.datetime === inputValue);
+    
+    console.log(filteredData);
 });
